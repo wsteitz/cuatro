e61e54e6b04b1be4f3f18a88bfa9a584e6163f39
Wolfgang Steitz <wsteitz@gmail.com>
2015-03-01 18:15:21 +0100

    initial commit

---
+++ cuatro.py
@@ -0,0 +1,152 @@
+from collections import defaultdict
+import random
+import copy
+from texttable import Texttable
+
+from fields import *
+
+
+class Dice:
+    
+    def __init__(self):
+        self.throws = 0
+        
+    def roll(self, keep=None):
+        self.throws += 1
+        if keep is not None:
+            to_throw = 5 - len(keep)
+        else: 
+            to_throw = 5
+            keep = []
+        self.dices = keep + [random.randint(1, 6) for i in range(to_throw)]
+        self.update_counts()
+        
+    def update_counts(self):
+        self.counts = defaultdict(int)
+        for num in self.dices:
+            self.counts[num] += 1
+            
+    def verify(self, keep):
+        verified = []
+        available = self.dices[:]
+        for k in keep:
+            if k in available:
+                verified.append(k)
+                available.remove(k)
+        return verified
+        
+    def __repr__(self):
+        return " ".join([str(d) for d in sorted(self.dices)])
+            
+
+#FIXME correct board layout
+class Board:
+    
+    def __init__(self):
+        self.matrix = [[ThreeOfAKind(), FourOfAKind(), ThreeOfAKind(), One, Two],
+                       [Three, Street(), FullHouse(), Four, FiveOfAKind()],
+                       [FiveOfAKind(), ThreeOfAKind(), FourOfAKind(), Street(), FullHouse()],
+                       [FourOfAKind(), ThreeOfAKind(), Five, ThreeOfAKind(), FourOfAKind()],
+                       [FourOfAKind(), Street(), FiveOfAKind(), FullHouse(), Six]
+                      ]
+        
+    def place(self, place, player, dice):
+        field = self.matrix[place[0]][place[1]]
+        field.place(player, dice)
+        
+    def __repr__(self):
+        table = Texttable()
+        table.set_cols_align(["c"] * len(self.matrix))
+        for row in self.matrix:
+            items = []
+            for field in row:
+                items.append("%s\n\n%s\n%i" % (field.name, field.player, field.height))
+            table.add_row(items)
+        return table.draw()
+        
+                        
+#FIXME keep track of players stones left            
+class Game:
+    
+    def __init__(self):
+        self.players = []
+                
+    def add_player(self, player):
+        self.players.append(player)
+    
+    def start(self):
+        self.board = Board()
+        self.rounds = 0
+        self.winner = self.loop()
+        print self.winner.name, "wins the game in", self.rounds, "rounds"
+        
+    def loop(self):
+        while True:
+            self.rounds += 1
+            for player in self.players:
+                self.turn(player)
+                if self.is_finished():
+                    return player
+                
+    def is_finished(self):
+        print "not implemented"
+        return False
+    
+    def turn(self, player):
+        # pass a copy of the original board, so the players cannot cheat
+        board = copy.deepcopy(self.board)
+        dice = Dice()
+        keep = []
+        for throw in range(1, 5):
+            dice.roll(keep)
+            keep = player.play(dice, board)
+            # verify the answer of the player
+            keep = dice.verify(keep)
+            if len(keep) == 5:
+                break
+        # last roll, nothing for the player to decide
+        dice.roll(keep)
+        # ask player where to place the stone
+        place = player.place(dice, board)
+        self.board.place(place, player, dice)
+        
+
+
+class Player:
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def play(self, *args):
+        print "not implemented"
+        return []
+    
+    def place(self, *args):
+        print "not implemented"
+            
+        
+class Human(Player):
+    
+    def __init__(self, name):
+        Player.__init__(self, name)
+     
+    def play(self, dice, board):
+        print board
+        print dice.throws, "\t", dice
+        keep = raw_input("which numbers do you want to keep? ")
+        return [int(k) for k in keep if k.isdigit()]
+    
+    def place(self, dice, board):
+        print board
+        print dice
+        place = raw_input("where to place your stone ('x y')? ")
+        place = [int(k) for k in place if k.isdigit()]
+        return place[0], place [1]
+        
+
+if __name__ == "__main__":
+    g = Game()
+    g.add_player(Human("Adam"))
+    #g.add_player(Human("Eve"))
+    g.start()
+    
---
+++ fields.py
@@ -0,0 +1,93 @@
+
+class Field:
+    
+    def __init__(self, name):
+        self.name = name
+        self.height = 0
+        self.player = None
+        
+    def place(self, player, dice):
+        if self.fits(dice):
+            self.height += 1
+            self.player = player.name
+    
+    def fits_height(self, dice):
+        return 5 - self.height >= dice.throws
+        
+    def __repr__(self):
+        return self.name
+
+
+class CountField(Field):
+    
+    def __init__(self, name, count):
+        Field.__init__(self, name)
+        self.count = count
+    
+    def fits(self, dice):
+        if max(dice.counts.values()) >= self.count:
+            return self.fits_height(dice)
+        return False
+
+
+class ThreeOfAKind(CountField):
+    
+    def __init__(self):
+        CountField.__init__(self, "ThreeOfAKind", 3)
+
+
+class FourOfAKind(CountField):
+    
+    def __init__(self):
+        CountField.__init__(self, "FourOfAKind", 4)
+
+
+class FiveOfAKind(CountField):
+    
+    def __init__(self):
+        CountField.__init__(self, "FiveOfAKind", 5)
+        
+        
+class FullHouse(Field):
+    
+    def __init__(self):
+        Field.__init__(self, "FullHouse")
+    
+    def fits(self, dice):
+        if max(dice.counts.values()) == 3 and min(dice.counts.values()) == 2:
+            return self.fits_height(dice)
+        return False 
+        
+        
+class Street(Field):
+    
+    def __init__(self):
+        Field.__init__(self, "Street")
+    
+    def fits(self, dice):
+        s = set(dice.dices)
+        if s == (1, 2, 3, 4, 5) or s == (2, 3, 4, 5, 6):
+            return self.fits_height(dice)
+        return False 
+
+
+class NumberField(Field):
+    
+    def __init__(self, name, number):
+        Field.__init__(self, name)
+        self.number = number
+    
+    def fits(self, dice):
+        if dice.counts[self.number] >= 2:
+            return self.fits_height(dice)
+        return False
+
+
+One = NumberField("Ones", 1)
+Two = NumberField("Twos", 2)
+Three = NumberField("Threes", 3)
+Four = NumberField("Fours", 4)
+Five = NumberField("Fives", 5)
+Six = NumberField("Sixes", 6)
+
+
